6fa682f728384c4e6f8d8e0286712f485b4d6995
raul <raulnoronhadev@gmail.com>
2025-10-19 16:01:38 -0300

    Created Side Bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import MiniDrawer from './components/layouts/MiniDrawer'
+import { Box, Grid, Paper, Typography } from "@mui/material";
+
 export default function App() {
 
   return (
     <>
-
+      <MiniDrawer></MiniDrawer>
+      <Box sx={{ display: "flex", height: "100vh", bgcolor: "#f7f7fb" }}></Box>
     </>
   )
 }

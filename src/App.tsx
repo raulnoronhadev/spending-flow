0be893679a5502a3bf3f7c4e9078fa695bc9792c
raul <raulnoronhadev@gmail.com>
2025-10-19 17:09:49 -0300

    Create initial Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import MiniDrawer from './components/layouts/MiniDrawer'
+import Header from './components/layouts/Header'
 import { Box, Grid, Paper, Typography } from "@mui/material";
 
 export default function App() {
@@ -6,7 +7,9 @@ export default function App() {
   return (
     <>
       <MiniDrawer></MiniDrawer>
-      <Box sx={{ display: "flex", height: "100vh", bgcolor: "#f7f7fb" }}></Box>
+      <Header />
+      <Box sx={{ display: "flex", height: "100vh", bgcolor: "#f7f7fb" }}>
+      </Box>
     </>
   )
 }

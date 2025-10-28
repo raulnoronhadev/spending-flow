0177c5629a7658ae1c867d35d9808db43b2c5a2f
raul <raulnoronhadev@gmail.com>
2025-10-27 22:42:04 -0300

    Project erased and restructured

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
-import MiniDrawer from './components/layouts/MiniDrawer'
-import Header from './components/layouts/Header'
-import { Box, Grid, Paper, Typography } from "@mui/material";
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { ColorModeContext, useMode } from './theme';
+import { Routes, Route } from "react-router-dom";
 
 export default function App() {
+  const [theme, colorMode] = useMode();
 
   return (
-    <>
-      <MiniDrawer></MiniDrawer>
-      <Header />
-      <Box sx={{ display: "flex", height: "100vh", bgcolor: "#f7f7fb" }}>
-      </Box>
-    </>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <Routes>
+              <Route path="/" />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   )
 }
-
